e6eee72af64e64276327755e55a802413372f5d9
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-05-16 16:11:48 +0900

    setup

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,9 @@
-import classNames from './App.module.css';
+import { useListTasks } from "./generated";
+import classNames from "./App.module.css";
 
 export const App = () => {
+  const { data, isFetched, error } = useListTasks();
+  console.log(data);
+
   return <h1 className={classNames.heading}>Hello React!</h1>;
 };
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,12 +1,19 @@
-import ReactDOM from 'react-dom/client';
-import { App } from './App';
-import { StrictMode } from 'react';
-import './styles/global.css';
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import { StrictMode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Axios from "axios";
+import "./styles/global.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = new QueryClient();
+Axios.defaults.baseURL = "http://localhost:8000";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
 );
