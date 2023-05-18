5067b26e646e606fc9c1769d05480efa77369862
yaharu <genga711@gmail.com>
2023-05-18 12:41:46 +0900

    add create task

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,10 +5,11 @@ import Box from '@mui/joy/Box';
 import Button from '@mui/joy/Button';
 import { useCallback, useState } from 'react';
 import Typography from '@mui/joy/Typography';
-import { Task, useListTasks } from './generated';
+import { Task, getListTasksQueryKey, useCreateTask, useListTasks } from './generated';
+import { useQueryClient } from 'react-query';
 
 const MainStyle = {
-  display: 'grid',
+   display: 'grid',
    alignItems: 'center', 
    width: '100%', 
    minWidth: '700px', 
@@ -38,6 +39,36 @@ export const App = () => {
   const onChangeTitle = useCallback((event: React.ChangeEvent<HTMLInputElement>) => setTitle(event.target.value), [title]);
   const { data, status, error } = useListTasks();
 
+  const client = useQueryClient();
+  const result = useCreateTask({
+    mutation: {
+      onSuccess: (result) => {
+        //useCreateTaskの中でpostが完了してからmutationが行われるので同期関数であるsetQueryData使える
+        client.setQueryData(getListTasksQueryKey(), (prevState: any) => {
+          console.log(result)
+          const prevTaskList = prevState.data.tasks;
+          const nextState = {
+            ...prevState,
+            data: { tasks: [...prevTaskList, result.data.task] },
+          };
+
+          return nextState;
+        });
+      },
+    },
+  });
+  //引数がなんで連想配列{}？→client.ts121行目をみると{mutation: ~, axios: ~}となっているので連想配列で渡してあげる。
+  //onSuccessはpromiseオブジェクトにチェインするメソッド。だから、引数にmutationFnに指定したメソッド(APIリクエスト)の結果が使える。
+  //setQueryDataはonSuccessなら使うよね。setQueryDataはリクエストは飛ばさず、キャッシュの部分的更新。。getリクエスト投げるのはfetchで、キャッシュを丸ごと更新
+  //mutate(x)の引数には、useMutation(mutationFn: (x) => {})のxに渡すことができる。
+  //今回はpostしない仕様だから。
+  //mutate()はuseMutateで定義したリソースに関する変更の処理を実行するもの。
+
+  const onClickCreateButton = () => {
+    result.mutate();
+    setTitle('');
+  }
+
   // const BoxTaskStyle = {
   //   display: 'grid',
   //   gridTemplateColumns: '0.3fr 2.4fr 0.3fr'
@@ -50,7 +81,7 @@ export const App = () => {
             onChange={onChangeTitle}
             value={title}
             startDecorator={<FormatListBulletedRoundedIcon />}
-            endDecorator={<Button>作成</Button>}
+            endDecorator={<Button onClick={onClickCreateButton} disabled={title.length > 0 ? false : true} >作成</Button>}
           />
         </Box>
         <Box style={BoxTasksStyle}>
