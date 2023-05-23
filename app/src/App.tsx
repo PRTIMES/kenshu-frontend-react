98315c67a9adea319a17a3ee7193f16f459b194d
toshiki.miyagawa <toshiki.miyagawa@M2023-001-prtadminnoMacBook-Pro.local>
2023-05-23 11:48:13 +0900

    practice2

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,27 @@
-import classNames from './App.module.css';
+import classNames from "./App.module.css";
+import { StrictMode } from "react";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchList = () =>
+  fetch("http://localhost:8000/api/tasks").then((res) => res.json());
 
 export const App = () => {
-  return <h1 className={classNames.heading}>Hello React!</h1>;
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["tasks"],
+    queryFn: fetchList,
+  });
+
+  if (isLoading || !data) return <p>Loading...</p>;
+
+  return (
+    <StrictMode>
+      <ul className={classNames.heading}>
+        {data.tasks.map((task) => (
+          <li className={classNames.title} key={task.id}>
+            {task.title}
+          </li>
+        ))}
+      </ul>
+    </StrictMode>
+  );
 };
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,12 +1,15 @@
-import ReactDOM from 'react-dom/client';
-import { App } from './App';
-import { StrictMode } from 'react';
-import './styles/global.css';
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import { StrictMode } from "react";
+import "./styles/global.css";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const queryClient = new QueryClient();
 
 root.render(
-  <StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </StrictMode>,
+  </QueryClientProvider>
 );
