c2de94a13b4e46d446b50bde7b3b350f1e69f8d7
toshiki.miyagawa <toshiki.miyagawa@M2023-001-prtadminnoMacBook-Pro.local>
2023-05-23 18:22:13 +0900

    ユーザーのアクションをトリガーにリクエストを送る

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,23 @@
 import classNames from "./App.module.css";
 import { StrictMode } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
+import axios from "axios";
 
 const fetchList = () =>
   fetch("http://localhost:8000/api/tasks").then((res) => res.json());
 
 export const App = () => {
+  const queryClient = useQueryClient();
+
   const { data, isLoading, error } = useQuery({
     queryKey: ["tasks"],
     queryFn: fetchList,
+  });
+
+  const mutation = useMutation({
+    mutationFn: () => {
+      return axios.post("http://localhost:8000/api/tasks");
+    },
   });
 
   if (isLoading || !data) return <p>Loading...</p>;
@@ -16,6 +25,14 @@ export const App = () => {
   return (
     <StrictMode>
       <ul className={classNames.heading}>
+        <button
+          className={classNames.button}
+          onClick={() => {
+            mutation.mutate();
+          }}
+        >
+          タスクの追加
+        </button>
         {data.tasks.map((task) => (
           <li className={classNames.title} key={task.id}>
             {task.title}
