6966c48025184fe5db908478a275483862b586e2
toshiki.miyagawa <toshiki.miyagawa@M2023-001-prtadminnoMacBook-Pro.local>
2023-05-24 11:39:36 +0900

    refetch関数の追加

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,36 +3,46 @@ import { StrictMode } from "react";
 import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
 import axios from "axios";
 
+// usequeryの非同期関数
 const fetchList = () =>
   fetch("http://localhost:8000/api/tasks").then((res) => res.json());
 
 export const App = () => {
   const queryClient = useQueryClient();
 
+  // データ取得
   const { data, isLoading, error } = useQuery({
     queryKey: ["tasks"],
     queryFn: fetchList,
   });
 
+  ///　ボタンが押されたらリクエスト送信
   const mutation = useMutation({
     mutationFn: () => {
       return axios.post("http://localhost:8000/api/tasks");
     },
   });
 
-  if (isLoading || !data) return <p>Loading...</p>;
+  const Refetch = async () => {
+    await queryClient.refetchQueries();
+  };
 
+  //dataが入っているかの判定
+  if (isLoading || !data) return <p>Loading...</p>;
   return (
     <StrictMode>
+      {/* タスクの追加ボタン */}
       <ul className={classNames.heading}>
         <button
           className={classNames.button}
           onClick={() => {
+            Refetch;
             mutation.mutate();
           }}
         >
           タスクの追加
         </button>
+        {/* データの表示 */}
         {data.tasks.map((task) => (
           <li className={classNames.title} key={task.id}>
             {task.title}
